65af70a712c781145532840a4f0be26bb39951d4
Max Walaschewski <mawalla@protonmail.com>
2022-01-16 14:30:43 +0100

    added random backgrounds with session storage

--- moinschwein/views/index.py
+++ moinschwein/views/index.py
@@ -1,3 +1,6 @@
+import random
+
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -21,7 +24,11 @@ class IndexView(CommonTemplateView):
         return redirect(reverse('dashboard'))
 
     def get_context_data(self, **kwargs):
+        random_number = str(random.randint(1, 3)).rjust(3, '0')
+        path = f'img/backgrounds/{random_number}.jpg'
+        image_url = staticfiles_storage.url(path)
         return {
             **super().get_context_data(**kwargs),
             'total_accusations': Accusation.objects.count(),
+            'image_url': image_url,
         }
